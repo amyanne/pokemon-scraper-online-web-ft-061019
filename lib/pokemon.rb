e4f3e00ab19a20ff14d570e6362c1e6be73b6c3f
Amy Westcott <amy.westcott@yahoo.com>
2019-07-29 17:53:44 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,50 @@
 class Pokemon
+  attr_accessor :name, :type, :db, :id
+  
+  @@all = []
+  
+  def initialize(id: nil, name:, type:, db:)
+     @id = id
+     self.name = name
+     self.type = type
+     self.db = db
+  end
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.save(name, type, db)
+    if self.name == name && self.type == type && self.db == db
+      self.update 
+    else 
+      new_pokemon = Pokemon.new(name: name, type: type, db: db)
+      sql_create = <<-SQL
+            INSERT INTO pokemon (name, type)  VALUES (?, ?);
+      SQL
+      new_pokemon.db.execute(sql_create, new_pokemon.name, new_pokemon.type)
+
+      sql_get_id = "SELECT last_insert_rowid() FROM pokemon;"
+      new_pokemon.id = new_pokemon.db.execute(sql_get_id)
+      @@all << self
+    end
+ end 
+ 
+ def update
+    sql_update = <<-SQL
+      UPDATE students SET name = ?, type = ?, db = ? WHERE pokemon.id = ?;
+    SQL
+
+    DB[:conn].execute(sql_update, self.name, self.type, self.db, self.id)
+  end
+  
+  def self.find(db, id)
+    sql = <<-SQL
+         SELECT * FROM pokemon WHERE id = ?;
+      SQL
+      DB[:conn].execute(sql, id).map do |row|
+        self.row[0]
+      end.first
+     
+    end 
 end
