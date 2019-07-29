1ade5c9d8900635a48a04306f7823b96c2e7e599
Amy Westcott <amy.westcott@yahoo.com>
2019-07-29 18:04:45 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -46,7 +46,7 @@ class Pokemon
             LIMIT 1
         SQL
         
-        DB[:conn].execute(sql,id).map do |row|
+        db.execute(sql,id).map do |row|
             self.new_from_db(row)
         end.first
     end
